982df7ba803f7225254cb7a55971ac0a1c5c6457
Rachel Uwamahoro <racheluwamahoro55@gmail.com>
2021-04-26 12:39:02 +0200

    additional links for the project

--- awwardsApp/urls.py
+++ awwardsApp/urls.py
@@ -9,7 +9,13 @@ urlpatterns = [
     url(r'^accounts/profile/', views.my_profile, name='my_profile'),
     url(r'register/',views.register, name='register'),
     url(r'project/(\d+)',views.rate_project,name='rate-project'),
-  
+    url(r'profile/(\d+)',views.profile,name='profile'),
+    url(r'my_profile',views.my_profile,name='my_profile'), 
+    url(r'^new/project$', views.new_project, name='new_project'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+
 
 ]
 if settings.DEBUG:
